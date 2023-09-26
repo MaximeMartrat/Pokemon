cf1e0ebe5b49ffd2cc110db311ab60bfdc508d32
MaximeMartrat <maximemartrat@outlook.fr>
2023-09-26 15:34:50 +0200

    refacto

--- PokeDock/php/index.php
+++ PokeDock/php/index.php
@@ -5,15 +5,3 @@ require_once('utils/init.php');
 require_once('utils/router.php');
 
 
-// var_dump($_SESSION['pokemons']);
-// $pickachu->subPV(50);
-// $pickachu->attaque($carapuce);
-
-// echo '<pre>';
-// print_r($pickachu);
-// echo '<br>';
-
-// print_r($carapuce);
-// echo '<br>';
-
-
--- PokeDock/php/utils/database.php
+++ PokeDock/php/utils/database.php
@@ -2,14 +2,17 @@
 
 function connectToDB()
 {
-    $host = "dbBB";
-    $user = "BeWeb";
-    $pass = "Pokemon34";
-    $dbname = "Pokemon";
+    $config = require('config/config.php');
+
+
+    $host = $config['db_host'];
+    $user = $config['db_user'];
+    $pass = $config['db_pass'];
+    $dbname = $config['db_name'];
     try {
         $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // echo "Connection a la DataBase reussi<br>";
+        // echo "Connexion à la base de données réussie !";
     } catch (PDOException $e) {
         echo "Erreur : " . $e->getMessage();
     }
